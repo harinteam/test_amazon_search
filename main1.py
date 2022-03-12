49c4a1faee19aa4bbb82ef14ca37e0cfbc238c4d
adityadenny <aditaydenny6@gmail.com>
2022-03-13 00:23:35 +0700

    add implicity wait

--- main1.py
+++ main1.py
@@ -1,4 +1,3 @@
-import time
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -6,6 +5,7 @@ from selenium.webdriver.common.by import By
 
 service = ChromeService(executable_path="/usr/local/bin/chromedriver")
 driver = webdriver.Chrome(service=service)
+driver.implicitly_wait(10)
 
 driver.get("https://www.amazon.com/")
 driver.maximize_window()
@@ -13,11 +13,10 @@ driver.maximize_window()
 search = driver.find_element(By.ID, "twotabsearchtextbox")
 search.send_keys('dress', Keys.ENTER)
 
-expected_text = '"dressss"'
+expected_text = '"dress"'
 # actual_text = driver.find_element(By.XPATH, "//span[@class='a-color-state a-text-bold']").text
 actual_text = driver.find_element(By.CSS_SELECTOR, ".a-color-state.a-text-bold").text
 
 assert expected_text == actual_text, f'Error. Expected text: {expected_text}, but actual_text: {actual_text}'
 
-# time.sleep(5)
-# driver.quit()
+driver.quit()
